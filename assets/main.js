74aac3867e81393d7aa2eede4a96b617963fe49c
BrisaAimee <brisaaimeerodriguez@gmail.com>
2025-09-24 20:22:54 -0300

    cración del obj viajero

---
+++ assets/main.js
@@ -0,0 +1,8 @@
+class Viajero {
+    constructor(nombre, destino, fecha, etiquetas) {
+        this.nombre = nombre;
+        this.destino = destino;
+        this.fecha = fecha;
+        this.etiquetas = etiquetas;
+    }
+}
