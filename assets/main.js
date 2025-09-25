43e8b2790a73a9e9ce98ef16b100c00f131b0d10
BrisaAimee <brisaaimeerodriguez@gmail.com>
2025-09-24 21:35:42 -0300

    calcular match

--- assets/main.js
+++ assets/main.js
@@ -10,12 +10,21 @@ class Viajero {
         // Lógica para calcular el match entre viajeros
         const misEtiquetas = ["aventurero", "fiestero"];
         let coincidencias = 0;
+
+        for (let i = 0; i < this.etiquetas.length; i++) { //El doble for lo puse porque estaba comparando cada etiqueta del viajero con cada etiqueta mia
+            for (let j = 0; j < misEtiquetas.length; j++) {
+                if (this.etiquetas[i] === misEtiquetas[j]) {
+                    coincidencias++;
+                }
+            }
+        }
+        return coincidencias;
     }
 }
 
 const viajeros = [
-  new Viajero("Laura", "España", "2025-10-05", ["aventurero", "fiestero"]),
-  new Viajero("Carlos", "Canada", "2025-10-05", ["tranquilo", "aventurero"]),
-  new Viajero("Sofía", "Italia", "2025-11-10", ["fiestero", "tranquilo"]),
-  new Viajero("Tomás", "España", "2025-10-05", ["aventurero"]),
+    new Viajero("Laura", "España", "2025-10-05", ["aventurero", "fiestero"]),
+    new Viajero("Carlos", "Canada", "2025-10-05", ["tranquilo", "aventurero"]),
+    new Viajero("Sofía", "Italia", "2025-11-10", ["fiestero", "tranquilo"]),
+    new Viajero("Tomás", "España", "2025-10-05", ["aventurero"]),
 ];
