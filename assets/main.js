0179b6553ba62ac6093359e978833f9e981d2083
BrisaAimee <brisaaimeerodriguez@gmail.com>
2025-09-24 21:18:22 -0300

    me di cuenta que tenia que poner el metodo en el objeto y no ponerlo como una función afuera

--- assets/main.js
+++ assets/main.js
@@ -5,4 +5,17 @@ class Viajero {
         this.fecha = fecha;
         this.etiquetas = etiquetas;
     }
+
+    calcularMatch() {
+        // Lógica para calcular el match entre viajeros
+        const misEtiquetas = ["aventurero", "fiestero"];
+        let coincidencias = 0;
+    }
 }
+
+const viajeros = [
+  new Viajero("Laura", "España", "2025-10-05", ["aventurero", "fiestero"]),
+  new Viajero("Carlos", "Canada", "2025-10-05", ["tranquilo", "aventurero"]),
+  new Viajero("Sofía", "Italia", "2025-11-10", ["fiestero", "tranquilo"]),
+  new Viajero("Tomás", "España", "2025-10-05", ["aventurero"]),
+];
